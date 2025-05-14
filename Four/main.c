225af22bba6fef7a575dc0f876e170b813af505c
valore <159122707+varedias@users.noreply.github.com>
2025-05-14 16:34:39 +0800

    Update main.c

--- Four/main.c
+++ Four/main.c
@@ -1,18 +1,20 @@
+
 #include "stm32f10x.h"
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 #define BUFFER_SIZE 64
+#define MAX_RESPONSE_SIZE 32
 
 char rxBuffer[BUFFER_SIZE];
-uint8_t bufferIndex = 0;
-uint8_t expressionComplete = 0;
+volatile uint8_t bufferIndex = 0;
+volatile bool expressionComplete = false;
 
 void AllInit()
 {
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC | RCC_APB2Periph_USART1, ENABLE);
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC | RCC_APB2Periph_USART1, ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 
 
@@ -49,8 +51,7 @@ void AllInit()
     USART_ITConfig(USART1, USART_IT_RXNE, ENABLE); 
 }
 
-
-void USART_SendString(USART_TypeDef* USARTx, char *str)
+void USART_SendString(USART_TypeDef* USARTx, const char *str)
 {
     while(*str)
     {
@@ -59,23 +60,48 @@ void USART_SendString(USART_TypeDef* USARTx, char *str)
     }
 }
 
+bool ParseExpression(const char *expr, int *num1, char *op, int *num2)
+{
+    const char *ptr = expr;
+    
+    while(*ptr && isspace(*ptr)) ptr++;
 
-void CalculateExpression(char *expr)
+    if(!isdigit(*ptr)) return false;
+    *num1 = atoi(ptr);
+    
+    while(*ptr && isdigit(*ptr)) ptr++;
+    
+    
+    while(*ptr && isspace(*ptr)) ptr++;
+    
+    
+    if(!(*ptr == '+' || *ptr == '-' || *ptr == '*' || 
+         *ptr == '/' || *ptr == '%' || *ptr == '&' || *ptr == '|'))
+        return false;
+    *op = *ptr++;
+    
+    
+    while(*ptr && isspace(*ptr)) ptr++;
+    
+    
+    if(!isdigit(*ptr)) return false;
+    *num2 = atoi(ptr);
+    
+    return true;
+}
+
+void CalculateExpression(const char *expr)
 {
     int num1 = 0, num2 = 0, result = 0;
     char op = 0;
-    char *ptr = expr;
-
-    while(*ptr && !isdigit(*ptr)) ptr++; 
-    num1 = atoi(ptr);
+    char response[MAX_RESPONSE_SIZE];
     
-  
-    while(*ptr && isdigit(*ptr)) ptr++;
-    op = *ptr;
-    ptr++;
+    if(!ParseExpression(expr, &num1, &op, &num2))
+    {
+        USART_SendString(USART1, "Error: Invalid expression format\r\n");
+        return;
+    }
     
-    num2 = atoi(ptr);
-
     switch(op)
     {
         case '+':
@@ -88,12 +114,19 @@ void CalculateExpression(char *expr)
             result = num1 * num2;
             break;
         case '/':
-            if(num2 != 0)
-                result = num1 / num2;
-            else
+            if(num2 == 0)
+            {
                 USART_SendString(USART1, "Error: Division by zero\r\n");
+                return;
+            }
+            result = num1 / num2;
             break;
         case '%':
+            if(num2 == 0)
+            {
+                USART_SendString(USART1, "Error: Modulo by zero\r\n");
+                return;
+            }
             result = num1 % num2;
             break;
         case '&':
@@ -103,57 +136,65 @@ void CalculateExpression(char *expr)
             result = num1 | num2;
             break;
         default:
-            USART_SendString(USART1, "Error: Invalid operator\r\n");
+            USART_SendString(USART1, "Error: Unsupported operator\r\n");
             return;
     }
     
-    // ??????????
-    char response[32];
-    sprintf(response, "%d%c%d=%d\r\n", num1, op, num2, result);
+    snprintf(response, MAX_RESPONSE_SIZE, "%d %c %d = %d\r\n", num1, op, num2, result);
     USART_SendString(USART1, response);
 }
-
 
 void USART1_IRQHandler(void)
 {
     if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
     {
         uint8_t receivedData = USART_ReceiveData(USART1);
-        
 
         if(receivedData == '\r' || receivedData == '\n')
         {
             if(bufferIndex > 0) 
             {
-                rxBuffer[bufferIndex] = '\0'; 
-                expressionComplete = 1;
+                rxBuffer[bufferIndex] = '\0';
+                expressionComplete = true;
                 bufferIndex = 0;
             }
         }
-        else if(bufferIndex < BUFFER_SIZE - 1) 
+
+        else if(receivedData == '\b' || receivedData == 0x7F)
+        {
+            if(bufferIndex > 0)
+            {
+                bufferIndex--;
+            }
+        }
+
+        else if(bufferIndex < BUFFER_SIZE - 1 && isprint(receivedData)) 
         {
             rxBuffer[bufferIndex++] = receivedData;
         }
+
     }
 }
 
-int main()
+int main(void)
 {
     AllInit();
+    
+    USART_SendString(USART1, "Calculator ready. Enter expressions like '12+34' or '5 * 6'.\r\n");
     
     while(1)
     {
         if(expressionComplete)
         {
-
-            char *questionMark = strstr(rxBuffer, "?");
+            char *questionMark = strchr(rxBuffer, '?');
             if(questionMark != NULL)
             {
-                *questionMark = '\0'; 
+                *questionMark = '\0';
             }
             
             CalculateExpression(rxBuffer);
-            expressionComplete = 0;
+            expressionComplete = false;
         }
     }
 }
+
